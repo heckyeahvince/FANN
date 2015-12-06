963b1f4c04544d4b69036370f402f4328f206871
Paul Vince Abe <peeveeabe10@gmail.com>
2015-12-07 01:09:56 +0800

    Initial commit

---
+++ app/obj/local/armeabi-v7a/objs/fann/fann/fann.o.d
@@ -0,0 +1,137 @@
+obj/local/armeabi-v7a/objs/fann/fann/fann.o: jni/fann/fann.c \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/stdio.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/cdefs.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/android/api-level.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/types.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/stdint.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/_types.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/machine/_types.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/_wchar_limits.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/posix_types.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/stddef.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/compiler.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/asm/posix_types.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/asm/types.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/types.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/machine/kernel.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/sysmacros.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/stdlib.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/string.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/malloc.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/alloca.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/strings.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/memory.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/time.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/time.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/time.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/asm/siginfo.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/asm-generic/siginfo.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/math.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/limits.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/limits.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/limits.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/machine/internal_types.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/machine/limits.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/syslimits.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/asm/page.h \
+ jni/fann/include/config.h jni/fann/include/fann.h \
+ jni/fann/include/floatfann.h jni/fann/include/fann.h \
+ jni/fann/include/fann_error.h jni/fann/include/fann_activation.h \
+ jni/fann/include/fann_data.h jni/fann/include/fann_internal.h \
+ jni/fann/include/fann_train.h jni/fann/include/fann_cascade.h \
+ jni/fann/include/fann_io.h
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/stdio.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/cdefs.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/android/api-level.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/types.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/stdint.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/_types.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/machine/_types.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/_wchar_limits.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/posix_types.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/stddef.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/compiler.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/asm/posix_types.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/asm/types.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/types.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/machine/kernel.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/sysmacros.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/stdlib.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/string.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/malloc.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/alloca.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/strings.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/memory.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/time.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/time.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/time.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/asm/siginfo.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/asm-generic/siginfo.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/math.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/limits.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/limits.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/limits.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/machine/internal_types.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/machine/limits.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/syslimits.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/asm/page.h:
+
+jni/fann/include/config.h:
+
+jni/fann/include/fann.h:
+
+jni/fann/include/floatfann.h:
+
+jni/fann/include/fann.h:
+
+jni/fann/include/fann_error.h:
+
+jni/fann/include/fann_activation.h:
+
+jni/fann/include/fann_data.h:
+
+jni/fann/include/fann_internal.h:
+
+jni/fann/include/fann_train.h:
+
+jni/fann/include/fann_cascade.h:
+
+jni/fann/include/fann_io.h:
---
+++ app/obj/local/armeabi-v7a/objs/fixed-fann/fann/fixedfann.o.d
@@ -0,0 +1,155 @@
+obj/local/armeabi-v7a/objs/fixed-fann/fann/fixedfann.o: \
+ jni/fann/fixedfann.c jni/fann/include/config.h \
+ jni/fann/include/fixedfann.h jni/fann/include/fann.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/time.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/cdefs.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/android/api-level.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/types.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/stdint.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/_types.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/machine/_types.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/_wchar_limits.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/posix_types.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/stddef.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/compiler.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/asm/posix_types.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/asm/types.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/types.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/machine/kernel.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/sysmacros.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/time.h \
+ jni/fann/include/fann_error.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/stdio.h \
+ jni/fann/include/fann_activation.h jni/fann/include/fann_data.h \
+ jni/fann/include/fann_internal.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/math.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/limits.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/limits.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/limits.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/machine/internal_types.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/machine/limits.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/syslimits.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/asm/page.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/stdlib.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/string.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/malloc.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/alloca.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/strings.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/memory.h \
+ jni/fann/include/fann_train.h jni/fann/include/fann_cascade.h \
+ jni/fann/include/fann_io.h jni/fann/fann.c \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/time.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/asm/siginfo.h \
+ C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/asm-generic/siginfo.h \
+ jni/fann/include/fann.h jni/fann/fann_io.c jni/fann/include/fann_data.h \
+ jni/fann/fann_train.c jni/fann/fann_train_data.c jni/fann/fann_error.c \
+ jni/fann/fann_cascade.c
+
+jni/fann/include/config.h:
+
+jni/fann/include/fixedfann.h:
+
+jni/fann/include/fann.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/time.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/cdefs.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/android/api-level.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/types.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/stdint.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/_types.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/machine/_types.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/_wchar_limits.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/posix_types.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/stddef.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/compiler.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/asm/posix_types.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/asm/types.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/types.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/machine/kernel.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/sysmacros.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/time.h:
+
+jni/fann/include/fann_error.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/stdio.h:
+
+jni/fann/include/fann_activation.h:
+
+jni/fann/include/fann_data.h:
+
+jni/fann/include/fann_internal.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/math.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/limits.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/limits.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/linux/limits.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/machine/internal_types.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/machine/limits.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/sys/syslimits.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/asm/page.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/stdlib.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/string.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/malloc.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/alloca.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/strings.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/memory.h:
+
+jni/fann/include/fann_train.h:
+
+jni/fann/include/fann_cascade.h:
+
+jni/fann/include/fann_io.h:
+
+jni/fann/fann.c:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/time.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/asm/siginfo.h:
+
+C:/Users/heckyeahvince/AppData/Local/Android/ndk/platforms/android-15/arch-arm/usr/include/asm-generic/siginfo.h:
+
+jni/fann/include/fann.h:
+
+jni/fann/fann_io.c:
+
+jni/fann/include/fann_data.h:
+
+jni/fann/fann_train.c:
+
+jni/fann/fann_train_data.c:
+
+jni/fann/fann_error.c:
+
+jni/fann/fann_cascade.c:
